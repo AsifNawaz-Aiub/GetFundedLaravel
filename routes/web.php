de8aafadc3124f051bc1d50aa6bf9ecef298bd58
Asif Nawaz <asifnawaz5926@gmail.com>
2021-01-02 23:58:37 +0600

    Completed Profile and Event Tab in Moderator

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('moderator.index');
 });
+Route::get('/moderator', 'moderatorController@index');
 
 Route::get('/login', 'loginController@index');
 Route::post('/login', 'loginController@verify');
