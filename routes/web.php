86f353a8f5c691d57781a70e8115208d52e423ec
Asif Nawaz <asifnawaz5926@gmail.com>
2021-01-03 01:01:27 +0600

    added approve system and search in event tab

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,19 @@
 */
 
 Route::get('/', function () {
-    return view('moderator.index');
+    
 });
 Route::get('/moderator', 'moderatorController@index');
+
+Route::get('/approve/{id}', 'moderatorController@approve')->name('event.approve');
+Route::post('/approve/{id}', 'moderatorController@approved')->name('event.approved');
+
+Route::get('/modify/{id}', 'moderatorController@modify')->name('event.modify');
+Route::post('/modify/{id}', 'moderatorController@modified')->name('event.modified');;
+
+Route::get('/decline/{id}', 'moderatorController@decline')->name('event.decline');;
+Route::post('/decline/{id}', 'moderatorController@declined')->name('event.declined');;
+
 
 Route::get('/login', 'loginController@index');
 Route::post('/login', 'loginController@verify');
