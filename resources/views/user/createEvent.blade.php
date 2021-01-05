981d096ef6f806e86499d3853ed93fe7cb5f3308
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2021-01-05 17:07:58 +0600

    update create

--- resources/views/user/createEvent.blade.php
+++ resources/views/user/createEvent.blade.php
@@ -18,29 +18,29 @@ Create Event
 	  <h2>Create Event Form</h2>
 	  <form method="post">
 	    <div class="form-group">
-	      <label for="eventName">Event Name:</label>
+	      <label for="eventName"><b>Event Name:</b></label>
 	      <input type="text" class="form-control" id="eventName" placeholder="eventName..." name="eventName" value="{{old('eventName')}}">
 	    </div>
 	    <div class="form-group">
-	      <label for="eventName">Description:</label>
+	      <label for="eventName"><b>Description:</b></label>
 	      <input type="text" class="form-control" id="description" placeholder="description..." name="description" value="{{old('description')}}">
 	    </div>
 	    <div class="form-group">
-	      <label for="CategoryId">Category Id:</label><br>
+	      <label for="CategoryId"><b>Category:</b></label><br>
 			<input type="radio" name="categoryId" value="0"> Study 
 			<input type="radio" name="categoryId" value="1"> Health
 			<input type="radio" name="categoryId" value="2"> Social
 	    </div>
 	    <div class="form-group">
-	      <label for="eventName">Goal Amount:</label>
+	      <label for="eventName"><b>Goal Amount:</b></label>
 	      <input type="text" class="form-control" id="goalAmount" placeholder="goalAmount..." name="goalAmount" value="{{old('goalAmount')}}">
 	    </div>
 	    <div class="form-group">
-	      <label for="eventName">Goal Date:</label>
+	      <label for="eventName"><b>Goal Date:</b></label>
 	      <input type="date" class="form-control" id="goalDate" placeholder="goalDate..." name="goalDate" value="{{old('goalDate')}}">
 	    </div>
 	    <div class="form-group">
-	      <label for="Picture">Picture:</label>
+	      <label for="Picture"><b>Picture:</b></label>
 	      <input type="file" class="picture" id="eventPicture"  name="eventPicture">
         </div>
         <input type="submit" name="submit" class="btn btn-dark" value="Confirm">
